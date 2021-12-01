5a81ca2ad2dae6c1aec31104b4ddb34dc1934cf5
Joe Tebbett <jmt631@cs.bham.ac.uk>
2021-12-01 17:53:13 +0000

    Refactored Spotify.js. Functions now return promises instead of using callbacks. Added functions to get all tracks from a user's library and features for those tracks. Removed elements from App.js that I'm not currently working on (CSV upload/graph view/playlist selector). Started work on UI for playlist recipe feature.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import React, { useState, Component } from 'react';
-import { CSVReader } from 'react-papaparse';
+import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import Graph, { makeNodesFromCSV } from './Backend.js';
-import { getPlaylistByID, SpotifyLogin, PlaylistSelector } from './Spotify.js';
-import './skeleton.css';
-
-// Import the functions you need from the SDKs you need
-
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCu-NQGPK9uPbhdLhodpEDfShgCaEFIo6A",
-//   authDomain: "playvis.firebaseapp.com",
-//   projectId: "playvis",
-//   storageBucket: "playvis.appspot.com",
-//   messagingSenderId: "129615699475",
-//   appId: "1:129615699475:web:f98fae195b6106bcec69e6",
-//   measurementId: "G-NB8C4XW1EQ"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-const buttonRef = React.createRef();
+import { getFeatures, getAllUserTracks, SpotifyLogin } from './Spotify.js';
+import './index.css'
 
 export default function App() {
   const [token, setToken] = useState(Cookies.get("spotifyAuthToken"));
   const [songs, setSongs] = useState();
-  const onPlaylistSelect = (e) => {
-    const id = e.target.value;
-    getPlaylistByID(id, token, setSongs);
-  };
-  const onFileUpload = (file) => { 
-    setSongs(makeNodesFromCSV(file)) 
-  };
+  const [features, setFeatures] = useState();
+  const [recipeSteps, setSteps] = useState([]);
+
+  useEffect(() => {
+    if (recipeSteps.length === 0) {
+      for (let i = 0; i < 3; i++)
+        recipeSteps.push({ bpm: i, acous: i, dance: i, key: i })
+    }
+  }, [recipeSteps]);
+
+  useEffect(() => {
+    if (token) {
+      async function fetchData() {
+        const asongs = await getAllUserTracks(token)
+        setSongs(asongs)
+        const afeatures = await getFeatures(asongs, token)
+        setFeatures(afeatures)
+      }
+      fetchData()
+    }
+  }, [token]);
+
+  const addStep = () => {
+    setSteps(steps => [...steps, { bpm: 50, acous: 50, dance: 50, key: steps.length + 1 }])
+  }
+
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>playvis</h1>
-      <h6 style={{ textAlign: 'center' }}>Upload a .csv file with playlist data or login with Spotify</h6>
       <div style={{
         display: 'flex',
         flexDirection: 'column',
@@ -47,95 +42,37 @@ export default function App() {
         justifyContent: 'center'
       }}>
         <SpotifyLogin token={token} setToken={setToken} />
-        {token &&
-          <PlaylistSelector token={token} onChange={onPlaylistSelect} />
-        }
+        {recipeSteps.map((vals) => <RecipeStep vals={vals} key={vals.key} />)}
+        <button onClick={addStep}>Add step</button>
       </div>
-      <CSVReader1 onFileUpload={onFileUpload} />
-      {songs && <Graph nodes={songs} />}
     </>
   );
 }
-class CSVReader1 extends Component {
 
-  handleOpenDialog = (e) => {
-    // Note that the ref is set async, so it might be null at some point
-    if (buttonRef.current) {
-      buttonRef.current.open(e);
-    }
-  };
+const RecipeStep = (props) => {
 
-  handleOnFileLoad = (data) => {
-    this.props.onFileUpload(data);
-  };
-
-  handleRemoveFile = (e) => {
-    // Note that the ref is set async, so it might be null at some point
-    if (buttonRef.current) {
-      buttonRef.current.removeFile(e);
-    }
-  };
-
-  render() {
-    return (
-      <>
-        <CSVReader
-          ref={buttonRef}
-          onFileLoad={this.handleOnFileLoad}
-          onError={this.handleOnError}
-          parserOptions={{ header: true }} // doesn't seem to work
-          noClick
-          noDrag
-          onRemoveFile={this.handleOnRemoveFile}
-        >
-          {({ file }) => (
-            <aside
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                gap: 10,
-                marginBottom: 10,
-              }}
-            >
-              <button
-                type="button"
-                onClick={this.handleOpenDialog}
-              >
-                Browse file
-              </button>
-              <div style={{
-                display: "inline-block",
-                height: "38px",
-                width: "40%",
-                padding: "0 30px",
-                color: "#555",
-                fontSize: "11px",
-                fontWeight: 600,
-                lineHeight: "38px",
-                textAlign: "center",
-                letterSpacing: ".1rem",
-                textTransform: "uppercase",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                backgroundColor: "transparent",
-                borderRadius: "4px",
-                border: "1px solid #bbb",
-                boxSizing: "border-box"
-              }}>
-                {file && file.name}
-              </div>
-              <button
-                onClick={this.handleRemoveFile}
-              >
-                Remove
-              </button>
-            </aside>
-          )}
-        </CSVReader>
-      </>
-    );
-  }
+  return (
+    <div className="step">
+      <form>
+        <label className="stepelem">
+          BPM
+          <input type="range" />
+        </label>
+        <label className="stepelem">
+          Acousticness
+          <input type="range" />
+        </label>
+        <label className="stepelem">
+          Danceability
+          <input type="range" />
+        </label>
+      </form>
+    </div>
+  );
 }
+
+
+
+
 
 
--- src/Spotify.js
+++ src/Spotify.js
@@ -4,44 +4,93 @@ import { SpotifyAuth, Scopes } from 'react-spotify-auth';
 import SpotifyWebApi from 'spotify-web-api-js';
 import 'react-spotify-auth/dist/index.css'
 
-async function getArtists(ids, api) {
-  let artists = [];
-  const promises = ids.map((id) => api.getArtist(id));
-  artists = await Promise.all(promises);
-  return artists;
+const simpleTrackObj = (track, artist) => {
+  const genre = (artist.genres.length) === 0 ? "no genre listed" : artist.genres[0];
+  return {
+    title: track.name,
+    artist: track.artists[0].name,
+    genre: genre
+  }
 }
 
-export const getPlaylistByID = (id, token, setSongs) => {
+export const getPlaylistByID = (id, token) => {
   const spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken(token);
 
-  var p_tracks = spotifyApi.getPlaylist(id).then(
-    (data) =>  data.tracks.items.map((item) => item.track)
+  const p_tracks = spotifyApi.getPlaylist(id).then(
+    ({ tracks }) => tracks.items.map((item) => item.track)
   );
-  var p_artists = p_tracks.then(
-    (tracks) =>  getArtists(tracks.map((track) => track.artists[0].id), spotifyApi)
+
+  const p_artists = p_tracks.then(
+    (tracks) => Promise.all(tracks.map((track) => spotifyApi.getArtist(track.artists[0].id)))
   );
-  Promise.all([p_tracks, p_artists]).then(function ([tracks, artists]) {
-    setSongs(
-      tracks.map((track, i) => {
-        const genre = (artists[i].genres.length) === 0 ? "no genre listed" : artists[i].genres[0];
-        return {
-          title: track.name,
-          artist: track.artists[0].name,
-          genre: genre
-        }
-      })
+
+  return Promise.all([p_tracks, p_artists]).then(function ([tracks, artists]) {
+    return tracks.map((track, i) => simpleTrackObj(track, artists[i])
     )
   })
 }
 
-export const PlaylistSelector = (props) => {
+export const TrackList = ({ songs }) => {
   return (
-    <SpotifyApiContext.Provider value={props.token}>
+    //JSON.stringify(songs)
+    <h3>tracks go here</h3>
+    //songs.map((item) => <span>{item.track.name}</span>)
+
+  )
+}
+
+export const getFeatures = (songs, token) => {
+  const spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(token)
+
+  const limit = 50;
+
+  let promises = [];
+
+  for (let i = 0; i < songs.length; i += limit)
+    promises.push(spotifyApi.getAudioFeaturesForTracks(
+      songs.slice(i, i + limit).map((song) => song.track.id)))
+
+  return Promise.all(promises)
+  .then((featuresList) => 
+      [].concat.apply([], featuresList.map((data) => data.audio_features))
+  )
+}
+
+export const getAllUserTracks = (token) => {
+  const spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(token);
+
+  const limit = 50;
+
+  // get all tracks in 50 track chunks (limited by spotify API)
+  return spotifyApi.getMySavedTracks()
+    .then((data) => data.total)
+    .then((total) => {
+      let promises = []
+      // i < 200 because of spotify rate limiting
+      for (let i = 0; i < total && i < 200; i += limit)
+        promises.push(
+          spotifyApi.getMySavedTracks({ offset: i, limit: limit })
+            .then((data) => data.items)
+        )
+      return Promise.all(promises)
+    })
+    .then((trackLists) =>
+      //console.log(trackLists)
+      //setSongs([].concat.apply([], trackLists))
+      [].concat.apply([], trackLists)
+    )
+}
+
+export const PlaylistSelector = ({ token, onChange }) => {
+  return (
+    <SpotifyApiContext.Provider value={token}>
       <UserPlaylists>
         {({ data }) =>
           data ? (
-            <select onChange={props.onChange}>
+            <select onChange={onChange}>
               {data.items.map((item) => (
                 <option key={item.id} value={item.id}>{item.name}</option>))
               }
@@ -53,11 +102,11 @@ export const PlaylistSelector = (props) => {
   )
 }
 
-export const SpotifyLogin = (props) => {
+export const SpotifyLogin = ({ token, setToken }) => {
   return (
     <div>
-      {props.token ? (
-        <SpotifyApiContext.Provider value={props.token}>
+      {token ? (
+        <SpotifyApiContext.Provider value={token}>
           <User>
             {({ data }) =>
               data ? (
@@ -72,12 +121,10 @@ export const SpotifyLogin = (props) => {
           redirectUri='http://localhost:3000/'
           //redirectUri='https://playvis.web.app/'
           clientID='c79989282f4f40a2953b4adc36489afc'
-          scopes={[Scopes.playlistReadCollaborative, Scopes.playlistReadCollaborative]}
-          onAccessToken={
-            (token) => {
-              props.setToken(token);
-
-            }}
+          scopes={
+            [Scopes.playlistReadCollaborative,
+            Scopes.userLibraryRead]}
+          onAccessToken={(t) => setToken(t)}
         />
       )}
     </div>
